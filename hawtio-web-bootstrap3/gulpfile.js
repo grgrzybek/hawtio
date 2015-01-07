aa25264dc54daaf7814838f3d4066dda93dd3e80
Grzegorz Grzybek <gr.grzybek@gmail.com>
2015-01-07 11:54:58 +0100

    Updates to dependencies, cleaning gulpfile.js

--- hawtio-web-bootstrap3/gulpfile.js
+++ hawtio-web-bootstrap3/gulpfile.js
@@ -4,23 +4,42 @@ var gulp = require("gulp");
 var plugins = require("gulp-load-plugins")({ lazy: false });
 
 
+// http://blog.nodejitsu.com/npmawesome-9-gulp-plugins/
+// https://github.com/gulpjs/gulp-util
+// https://github.com/wearefractal/gulp-concat
+// https://github.com/terinjokes/gulp-uglify
+// https://github.com/hparra/gulp-rename
+// https://github.com/Metrime/gulp-filesize
+// https://github.com/plus3network/gulp-less
+// https://github.com/sindresorhus/gulp-changed
+// https://github.com/floatdrop/gulp-watch
+
 // Default
 gulp.task("default", [ "bower" ], function() {
   require("run-sequence")("typescript-files", "test");
 });
 
+
+// Clean
+// https://github.com/peter-vilja/gulp-clean
+gulp.task("clean", function () {
+  return gulp.src("dist", { read: false }).pipe(plugins.clean());
+});
+
+
 // Bower
 // https://www.npmjs.com/package/gulp-bower
 gulp.task("bower", function () {
-  plugins["bower"]();
+  plugins.bower();
 });
+
 
 // TypeScript compilation
 // https://www.npmjs.com/package/gulp-typescript
-var ts = plugins["typescript"];
+var ts = plugins.typescript;
 var eventStream = require("event-stream");
 // https://github.com/floridoo/gulp-sourcemaps
-var sourcemaps = plugins["sourcemaps"];
+var sourcemaps = plugins.sourcemaps;
 
 var tsProject = ts.createProject({
   removeComments: true,
@@ -39,11 +58,11 @@ gulp.task("typescript-files", function () {
 
   return eventStream.merge(
     tsResult.js
-        .pipe(plugins["concatSourcemap"]("app.js"))
+        .pipe(plugins.concatSourcemap("app.js"))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest("src/main/webapp/app/")),
     tsResult.dts
-        .pipe(plugins["concat"]("app.d.ts"))
+        .pipe(plugins.concat("app.d.ts"))
         //.pipe(gulp.dest("src/main/webapp/app/"))
   );
 });
@@ -59,52 +78,55 @@ gulp.task("watch", [ "typescript-files" ], function () {
 // middlewares
 var redirect = require("redirecter");
 
+// minimal Jolokia server
+var jolokia = function (req, res, next) {
+  if (/^\/jolokia/.test(req.url)) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(JSON.stringify({
+      "timestamp": 1417766745,
+      "status": 200,
+      "request": {"type": "version"},
+      "value": {
+        "protocol": "7.2",
+        "config": {"agentId": "agentid", "agentType": "servlet"},
+        "agent": "1.2.3",
+        "info": {}
+      }
+    }, null, 0));
+    res.end();
+  } else {
+    next();
+  }
+};
+
+// redirect non-resource requests to index.html (https://code.angularjs.org/1.3.5/docs/guide/$location#server-side)
+var redirecter = function (req, res, next) {
+  var lastPath = req.url.split("/").pop();
+  var last = lastPath.split("?", 1)[0];
+  if (last !== "" && !/\.[a-z0-9]+$/.test(last)) {
+    console.log("Redirecting: " + req.url);
+    redirect(req, res, "/");
+  } else {
+    // just invoke next middleware
+    next();
+  }
+};
+
 gulp.task("server", function () {
   gulp.src("src/main/webapp")
-      .pipe(plugins["webserver"]({
+      .pipe(plugins.webserver({
         port: 9009,
         livereload: false,
         directoryListing: false,
         open: false,
+        // we can use the following middleware:
+        // * https://github.com/senchalabs/connect#user-content-middleware
+        // * https://github.com/Raynos/http-framework/wiki/Modules
+        // * generic middlewares in the form of "function(req, res, next) {}"
+        // for what you can do with req and res, see http://nodejs.org/api/http.html
         middleware: [
-          // we can use the following middleware:
-          // * https://github.com/senchalabs/connect#user-content-middleware
-          // * https://github.com/Raynos/http-framework/wiki/Modules
-          // * generic middlewares in the form of "function(req, res, next) {}"
-          // for what you can do with req and res, see http://nodejs.org/api/http.html
-
-          // minimal Jolokia server
-          function (req, res, next) {
-            if (/^\/jolokia/.test(req.url)) {
-              res.writeHead(200, { "Content-Type": "application/json" });
-              res.write(JSON.stringify({
-                "timestamp": 1417766745,
-                "status": 200,
-                "request": {"type": "version"},
-                "value": {
-                  "protocol": "7.2",
-                  "config": {"agentId": "agentid", "agentType": "servlet"},
-                  "agent": "1.2.3",
-                  "info": {}
-                }
-              }, null, 0));
-              res.end();
-            } else {
-              next();
-            }
-          },
-          // redirect non-resource requests to index.html (https://code.angularjs.org/1.3.5/docs/guide/$location#server-side)
-          function (req, res, next) {
-            var lastPath = req.url.split("/").pop();
-            var last = lastPath.split("?", 1)[0];
-            if (last !== "" && !/\.[a-z0-9]+$/.test(last)) {
-              console.log("Redirecting: " + req.url);
-              redirect(req, res, "/");
-            } else {
-              // just invoke next middleware (created from options.bases)
-              next();
-            }
-          }
+          jolokia,
+          redirecter
         ]
       }));
 });
